3b7055424b9b7f43d846e9b0207ba140d5ca518b
LePichu <ishat.prsnl65844@gmail.com>
2023-03-01 01:05:00 +0530

    fix: home and vale icon

--- src/_includes/components/Home.tsx
+++ src/_includes/components/Home.tsx
@@ -2,18 +2,21 @@ export default function Home() {
     return (
         <div id="home">
             <img src="/assets/icon.png" />
+            <br /><br />
             <h1>
                 Hi, I do things.
             </h1>
-            <br /><br />
-            {Object.entries({
-                "Learn More": "/about",
-                "Read My Ramblings": "/blog",
-            }).map(([k, v]) => {
-                return <a href={v}>
-                    {k}
-                </a>
-            })}
+            <br />
+            <div>
+                {Object.entries({
+                    "Learn More": "/about",
+                    "Read My Ramblings": "/blog",
+                }).map(([k, v]) => {
+                    return <a href={v}>
+                        {k}
+                    </a>
+                })}
+            </div>
         </div>
     )
 }
